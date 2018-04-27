aec4642f91d1623104c56b6e673514d96faaafd8
21588204@qq.com <21588204@qq.com>
2018-04-26 22:22:03 -0600

    updates

--- contrainedSkyline/distanceToHotelsDist.py
+++ contrainedSkyline/distanceToHotelsDist.py
@@ -8,19 +8,27 @@ data_file = "/home/gqxwolf/mydata/pythonProject/hotels_distance.dat"
 
 with open(data_file) as f:
         content = f.readlines()
-        # you may also want to remove whitespace characters like `\n` at the end of each line
+# you may also want to remove whitespace characters like `\n` at the end of each line
 content = [x.strip() for x in content]
 
 
 dist_list=[i for i in range( int(content[-1].split(" ")[0])+1 )]
+
+sum=0
+for i in content:
+    sum = sum + int(i.split(" ")[1])
+print "total ",sum
 num_count=np.zeros(len(dist_list))
 
 #print content[-1]
 
+Expectation = 0;
+
 for line in  content:
     d_num = int(line.split(" ")[0])
     counter = int(line.split(" ")[-1])
-    num_count[d_num]=counter/25562
+    num_count[d_num]=counter/sum
+    Expectation = Expectation+d_num*num_count[d_num]
 
 
 #print len(num_count)
@@ -33,9 +41,9 @@ def plot_bar():
     #plt.text(0.05, 20, r'Expected Value = 4.212033487',fontsize=20)
     plt.xlim(0,len(dist_list))
     plt.ylabel("Probability")
-    plt.xlabel("Number of Bus Stops within a certain range (300m)")
+    plt.xlabel("Number of Bus Stops within a certain range")
 
-    textstr = 'Expected Value = 7.822079649'
+    textstr = 'Expected Value = '+ str(Expectation)
 
     props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 
