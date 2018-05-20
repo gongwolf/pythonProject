d7f39f703bd6d0c3ee7ba5f65ff6837fd5c53f79
21588204@qq.com <21588204@qq.com>
2018-05-20 16:54:48 -0600

    update

--- contrainedSkyline/analyzeResult1.py
+++ contrainedSkyline/analyzeResult1.py
@@ -12,13 +12,13 @@ def analyzeByFileName(file_name):
     
     
     op_base_time=0
-    #op_base_visisted_bus_stops=0
-    #op_base_final_bus_stops=0
+    op_base_visisted_bus_stops=0
+    op_base_final_bus_stops=0
     op_base_sk_counter=0
     
     base_5_time=0
-    #base_5_visisted_bus_stops=0
-    #base_5_final_bus_stops=0
+    base_5_visisted_bus_stops=0
+    base_5_final_bus_stops=0
     base_5_sk_counter=0
     
     
@@ -29,20 +29,18 @@ def analyzeByFileName(file_name):
         t_sk_counter=0
     
         t_time=float(line.split("|")[1])
-        #t_visisted_bus_stops=float(line.split(" ")[-4].split(",")[0])
-        #t_final_bus_stops=float(line.split(" ")[-4].split(",")[1])
         #t_sk_counter=float(line.split(" ")[-1])
         
         if counter <= number:
             op_base_time+=t_time
-            #op_base_visisted_bus_stops+=t_visisted_bus_stops
-            #op_base_final_bus_stops+=t_final_bus_stops
+            op_base_visisted_bus_stops+=float(line.split(" ")[-4].split(",")[0])
+            op_base_final_bus_stops+=float(line.split(" ")[-4].split(",")[1])
             op_base_sk_counter += float(line.split(" ")[-1])
     
         else:
             base_5_time+=t_time
-            #base_5_visisted_bus_stops+=t_visisted_bus_stops
-            #base_5_final_bus_stops+=t_final_bus_stops
+            base_5_visisted_bus_stops+=float(line.split(" ")[-5].split(",")[0])
+            base_5_final_bus_stops+=float(line.split(" ")[-5].split(",")[1])
             base_5_sk_counter += float(line.split(" ")[-2])
     
         counter+=1
@@ -50,24 +48,26 @@ def analyzeByFileName(file_name):
     
     
     op_base_time/=number
-    #op_base_visisted_bus_stops/=number
-    #op_base_final_bus_stops/=number
+    op_base_visisted_bus_stops/=number
+    op_base_final_bus_stops/=number
     op_base_sk_counter/=number
     
     base_5_time/=number
-    #base_5_visisted_bus_stops/=number
-    #base_5_final_bus_stops/=number
+    base_5_visisted_bus_stops/=number
+    base_5_final_bus_stops/=number
     base_5_sk_counter/=number
     
-    #base_t_ratio = base_5_final_bus_stops/base_5_visisted_bus_stops
+    base_t_ratio = base_5_final_bus_stops/base_5_visisted_bus_stops
+    op_t_ratio = op_base_final_bus_stops/op_base_visisted_bus_stops
     time_speed_up = op_base_time/base_5_time
     sk_speed_up = base_5_sk_counter/op_base_sk_counter*100
     
-    s="%.2f %.2f %.4f %d %d %.4f" % (base_5_time,op_base_time,time_speed_up,base_5_sk_counter,op_base_sk_counter,sk_speed_up)
+    s="%.2f %.2f %.4f %d %d %.4f %.2f %.2f" % (base_5_time,op_base_time,time_speed_up,base_5_sk_counter,op_base_sk_counter,sk_speed_up,base_t_ratio,op_t_ratio)
     return s
 
 
-path_base="/home/gqxwolf/shared_git/bConstrainSkyline/target/output/exp2/"
+path_base="/home/gqxwolf/shared_git/bConstrainSkyline/target/output/exp4/"
+#path_base="/home/gqxwolf/shared_git/bConstrainSkyline/target/output/exp3/"
 #path_base="/home/gqxwolf/shared_git/bConstrainSkyline/target/new_output/"
 
 
@@ -76,6 +76,5 @@ for file_name in os.listdir(path_base):
     graphsize = file_name.split("_")[0]
     degree = file_name.split("_")[1]
     hotels = file_name.split("_")[2]
-    #if int(graphsize) in [1000,2000,4000,6000] :
     s=analyzeByFileName(fname)
     print "%s %s %s %s" % (graphsize,degree,hotels,s)
--- contrainedSkyline/speedup.py
+++ contrainedSkyline/speedup.py
@@ -9,7 +9,7 @@ import json
 
 font = {'family' : 'Arial',
         'weight' : 'normal',
-        'size'   : 25}
+        'size'   : 30}
 matplotlib.rc('font', **font)
 axis_font = {'fontname':'Arial', 'size':'35'}
 
@@ -19,18 +19,30 @@ fig.set_figwidth(10)
 
 with open('speedup.txt') as infile:
     trend = numpy.loadtxt(infile)
-    print trend
+    # print trend
+
+with open('speedup_1.txt') as infile:
+    trend_1 = numpy.loadtxt(infile)
+    # print trend
+
+
 # y_max = max(trend[:,1])
 #plt.ylim([0, 2])
 
+#plt.plot(trend[:,0], trend[:,1], 'b^-', label='Varying graph size', ms=15, lw=5)
+#plt.plot(trend_1[:,0], trend_1[:,1], 'ro-', label='Varying # of object', ms=15, lw=5)
 #serial = 2449436.627
-plt.plot(trend[:,0], trend[:,2], 'b^-', label='ExactAlg-baseline', ms=10, lw=4)
-plt.plot(trend[:,0], trend[:,1], 'ro-', label='ExactAlg-improved', ms=10, lw=4)
+plt.plot(trend[:,0], trend[:,1], 'ro-', label='ExactAlg-baseline', ms=18, lw=5)
+# plt.plot(trend[:,0], trend[:,1], 'ro-', label='ExactAlg-improved', ms=18, lw=5)
 #plt.plot(trend[:,0], trend[:,0], 'ro-', ms=10, lw=4, label='Ideal parallel algorithm')
 # plt.plot(trend[:,0], serial/trend[:,1], 'b^-', ms=10, lw=4, label='Perfect')
-plt.xlabel('#k of Graph with 300 objects', **axis_font)
-plt.ylabel('Times to checking Skyline', **axis_font)
-plt.legend()
+plt.xlabel('|D|/N ', **axis_font)
+# plt.xlabel('average degree of the graph ', **axis_font)
+# plt.ylabel('Running Time in (Sec.)', **axis_font)
+# plt.ylabel('frequency of skyline checking', **axis_font)
+plt.ylabel('Visited Ratio', **axis_font)
+
+# plt.legend(loc=2)
 
 plt.savefig('speedup.pdf', bbox_inches='tight')
 # plt.show()
